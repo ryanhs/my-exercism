b5ad7eae561cb0aca64189706a1caae381e28958
ryanhs <mr.ryansilalahi@gmail.com>
2024-01-13 18:16:05 +0700

    sorting_room.go

---
+++ go/sorting-room/sorting_room.go
@@ -0,0 +1,67 @@
+package sorting
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// DescribeNumber should return a string describing the number.
+func DescribeNumber(f float64) string {
+	return fmt.Sprintf("This is the number %.1f", f)
+}
+
+type NumberBox interface {
+	Number() int
+}
+
+// DescribeNumberBox should return a string describing the NumberBox.
+func DescribeNumberBox(nb NumberBox) string {
+	return fmt.Sprintf("This is a box containing the number %.1f", float32(nb.Number()))
+}
+
+type FancyNumber struct {
+	n string
+}
+
+func (i FancyNumber) Value() string {
+	return i.n
+}
+
+type FancyNumberBox interface {
+	Value() string
+}
+
+// ExtractFancyNumber should return the integer value for a FancyNumber
+// and 0 if any other FancyNumberBox is supplied.
+func ExtractFancyNumber(fnb FancyNumberBox) int {
+	switch t := fnb.(type) {
+	case FancyNumber:
+		i, _ := strconv.Atoi(t.n)
+		return i
+	default:
+		return 0
+	}
+}
+
+// DescribeFancyNumberBox should return a string describing the FancyNumberBox.
+func DescribeFancyNumberBox(fnb FancyNumberBox) string {
+	return fmt.Sprintf("This is a fancy box containing the number %.1f", float32(ExtractFancyNumber(fnb)))
+}
+
+// DescribeAnything should return a string describing whatever it contains.
+func DescribeAnything(i interface{}) string {
+	switch t := i.(type) {
+	case FancyNumberBox:
+		return DescribeFancyNumberBox(t)
+	case NumberBox:
+		return DescribeNumberBox(t)
+	case float64:
+		return fmt.Sprintf("This is the number %.1f", float64(t))
+	case float32:
+		return fmt.Sprintf("This is the number %.1f", float64(t))
+	case int:
+		return fmt.Sprintf("This is the number %.1f", float64(t))
+	default:
+		return "Return to sender"
+	}
+}
